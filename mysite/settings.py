f19e281c9944d681b8a8f05f5ab87a3b3c9b871f
Bob Belderbos <bobbelderbos@gmail.com>
2022-06-14 11:29:05 +0200

    used the latest chart.js = simpler, more beautiful

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -116,14 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_HOST = ""
-STATIC_URL = STATIC_HOST + '/static/'
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- stats/views.py
+++ stats/views.py
@@ -6,11 +6,6 @@ from django.shortcuts import render
 from stats.models import BiteStat
 
 
-def _calculate_max_height_graph(values):
-    max_value = float(max(values))
-    return int(ceil(max_value / 100.0)) * 100
-
-
 def index(request):
     stats = BiteStat.objects.order_by('completed')
 
@@ -21,11 +16,9 @@ def index(request):
 
     # unpack dict keys / values into two lists
     labels, values = zip(*data.items())
-    max_height = _calculate_max_height_graph(values)
 
     context = {
         "labels": labels,
         "values": values,
-        "max_height": max_height,
     }
     return render(request, "graph.html", context)
