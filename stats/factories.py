ee1f8f6775196cd67e955476e2ab8698d61541d3
Kirk Davis <kirk5davis@gmail.com>
2022-06-20 00:24:00 -0700

    created fake stat generating command

--- stats/factories.py
+++ stats/factories.py
@@ -1,3 +1,5 @@
+import datetime
+
 import factory
 from factory.django import DjangoModelFactory
 
--- stats/management/commands/generate_fake_stats.py
+++ stats/management/commands/generate_fake_stats.py
@@ -1,2 +1,26 @@
+from django.core.management.base import BaseCommand
 
+from stats.models import BiteStat
+
+from stats.factories import BiteStatFactory
+
+class Command(BaseCommand):
+    help = 'Generate fake PyBites stats'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-n', '--num', type=int, required=True, help='Specify the number of fake records to create.')
+        parser.add_argument('-d', '--del', action='store_true', required=False, help='Flag for deleting existing data')
+
+    def handle(self, *args, **options):
+        number_of_records = options['num']
+        delete_flag = options['del']
+        if delete_flag:
+            self.stdout.write('Deleting existing data from BiteStat model...')
+            BiteStat.objects.all().delete()
+
+        self.stdout.write(f'Creating {number_of_records} new fake records...')
+        for record in range(number_of_records):
+            BiteStatFactory()
+        
+        self.stdout.write(f'Successfully created {number_of_records} new fake records.')
 
